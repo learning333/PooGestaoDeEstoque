1e2b9d165af6e2a279fdfe5f8f1787f49e09ff56
learning333 <simia.teste@gmail.com>
2021-07-20 12:58:38 -0300

    mudanca do banco de dados de mysql para h2

---
+++ src/main/java/br/com/ufabc/poo_gestao_de_estoque/controller/IndexController.java
@@ -0,0 +1,13 @@
+package br.com.ufabc.poo_gestao_de_estoque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
